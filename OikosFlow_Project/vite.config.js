724e11d3f2e5d488f570d8b1780194207336b328
Cardanivelez <carlosdanilovelez57@gmail.com>
2026-03-08 21:32:03 -0500

    feat(historial): navbar inferior fija

--- OikosFlow_Project/vite.config.js
+++ OikosFlow_Project/vite.config.js
@@ -5,7 +5,7 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   server: {
-    host: true,  // Permite acceso desde otras IPs
+    host: "0.0.0.0",  // 🔥 Permite conexiones externas
     port: 5173
   }
-})
+});
